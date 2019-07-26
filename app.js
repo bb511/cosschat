f897dd21b1db574b9972914bc2be4d40af5fbed1
myakoo <53275274+myakoo@users.noreply.github.com>
2019-07-26 20:06:57 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Chatkit from '@pusher/chatkit'
+import MessageList from './components/MessageList'
+import SendMessageForm from './components/SendMessageForm'
+import RoomList from './components/RoomList'
+import NewRoomForm from './components/NewRoomForm'
+
+
+class App extends React.Component {
+    
+    componentDidMount() {
+        const chatManager = new Chatkit.ChatManager({
+            
+        })
+    }
+    
+    render() {
+        return (
+            <div className="app">
+                <RoomList />
+                <MessageList />
+                <SendMessageForm />
+                <NewRoomForm />
+            </div>
+        );
+    }
+}
+
+export default App
