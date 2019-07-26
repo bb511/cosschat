9788829abbf6d6aa341f80d47efd52bcb56ff934
myakoo <53275274+myakoo@users.noreply.github.com>
2019-07-26 20:32:51 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/af799227-05d3-4a5b-ae27-8e378f5683b1/token";
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/927b7593-ada0-4a68-9144-9e5af2c35382/token";
 const instanceLocator = "v1:us1:927b7593-ada0-4a68-9144-9e5af2c35382";
 
 export { tokenUrl, instanceLocator }
