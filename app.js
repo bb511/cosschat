ae28781f7444b402188932e50326bca19d719fb1
myakoo <53275274+myakoo@users.noreply.github.com>
2019-07-26 20:31:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ import SendMessageForm from './components/SendMessageForm'
 import RoomList from './components/RoomList'
 import NewRoomForm from './components/NewRoomForm'
 
+import { tokenUrl, instanceLocator } from './config'
 
 class App extends React.Component {
     
     componentDidMount() {
         const chatManager = new Chatkit.ChatManager({
+            instanceLocator,
             
         })
     }
