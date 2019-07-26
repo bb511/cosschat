e793fa8b91f32d56dafcf1111e59408a42689c72
bb511 <patrickodagiu@gmail.com>
2019-07-26 23:42:57 +0200

    Cleaned up the website and made a repository structure.

--- config.js
+++
@@ -1,7 +0,0 @@
-/** swap out */
-const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/927b7593-ada0-4a68-9144-9e5af2c35382/token";
-
-/** swap out */
-const instanceLocator = "v1:us1:927b7593-ada0-4a68-9144-9e5af2c35382";
-
-export { tokenUrl, instanceLocator }
