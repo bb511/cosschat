79efa9a333e7583a2ee8d20c5cca8297a49b857d
myakoo <53275274+myakoo@users.noreply.github.com>
2019-07-27 03:09:02 +0200

    Update message.js

--- message.js
+++ message.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-class Message extends React.Component {  
-    render() {
+function Message (props) {  
         return (
             <div className="message">
+                  <div className="message-username">{props.username}</div>
+                  <div className="message-text">{props.text}</div>
             </div>
         )
     }
