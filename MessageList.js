8ed7573b89b4cb7bc43a6254295e2ad73323bd13
myakoo <53275274+myakoo@users.noreply.github.com>
2019-07-27 03:08:31 +0200

    Update MessageList.js

--- MessageList.js
+++ MessageList.js
@@ -1,16 +1,25 @@
 import React from 'react'
+import Message from './Message'
+import ReactDOM from './Message'
 
 class MessageList extends React.Component {
+    componentWillUpdate(){
+    	const node = ReactDOM.findDOMNode(this)
+    	this.shouldScrollToBottom = node.scrollTop + node.clientHeight + 100 >= node.scrollHeight
+    }
+    componeneDidUpdate () {
+	if (this.shouldScrollToBottom) {
+   	 	const node = ReactDOM.findDOMNode(this)   
+	 	node.scrollTop = node.scrollHeight
+	}
+    }
     render() {
         return (
             <div className="message-list">
                 {this.props.messages.map((message, index) => {
                     return (
-                        <div key={index} className="message">
-                            <div className="message-username">{message.senderId}</div>
-                            <div className="message-text">{message.text}</div>
-                        </div>
-                    )
+                        <Message key={index} username={message.senderId} text={message.text} />
+                         )
                 })}
             </div>
         )
