393b1eea3906d77dd24ed14f415bd4096dfd3eda
bb511 <patrickodagiu@gmail.com>
2019-07-28 04:18:22 +0200

    Added some minor things.

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import SignUpForm
 
 class SignUp(generic.CreateView):
     form_class = SignUpForm
-    success_url = reverse_lazy('accounts/login/')
+    success_url = reverse_lazy('accounts/login')
     template_name = 'signup.html'
